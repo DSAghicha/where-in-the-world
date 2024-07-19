560298488fbe5bd964015512f90d89a068de1016
Darshaan Aghicha <dsaghicha211@gmail.com>
2024-07-20 00:33:05 +0530

    Add country page navigation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import data from "../data/data.json";
 
 const Home = () => {
+    const navigate = useNavigate();
+
     return (
         <>
             <div className="items-cr">
                 {data.map((item, index) => (
-                    <div className="item" key={index}>
+                    <div
+                        className="item"
+                        key={index}
+                        onClick={() => navigate(`/${item.alpha3Code}`)}
+                    >
                         <img src={item.flag} alt={item.name} />
                         <h2>{item.name}</h2>
                         <div className="details">
